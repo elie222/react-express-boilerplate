011e37d2d65a4a23e1f00edef0d672a363efd3c7
Elie Steinbock <eliesteinbock@gmail.com>
2018-06-13 02:14:30 +0300

    added styled-components and theme provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
-import * as React from 'react';
-import MainLayout from "./layouts/MainLayout";
-import './App.scss';
+import * as React from 'react'
+import { ThemeProvider } from 'styled-components'
+import MainLayout from './layouts/MainLayout'
+import theme from './utils/theme';
+import './App.scss'
 
 class App extends React.Component {
   render() {
     return (
-      <MainLayout />
-    );
+      <ThemeProvider theme={theme}>
+        <MainLayout />
+      </ThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
