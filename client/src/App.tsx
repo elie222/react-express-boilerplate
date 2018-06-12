54751902c4f0fab7fb8eeb7052a4ff7fe4cd4bb3
Elie Steinbock <eliesteinbock@gmail.com>
2018-06-13 02:10:24 +0300

    added basic antd layout, husky and lint staged

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,11 @@
-import { Rate } from 'antd';
 import * as React from 'react';
+import MainLayout from "./layouts/MainLayout";
 import './App.scss';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
-  public render() {
+  render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Rate character="6"/>
-      </div>
+      <MainLayout />
     );
   }
 }
