616c523d7bac59f6517a5edc027f3e3bb6c54119
Elie Steinbock <eliesteinbock@gmail.com>
2018-06-15 02:58:50 +0300

    removed registration from main layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,12 @@ import { ThemeProvider } from 'styled-components'
 import MainLayout from './layouts/MainLayout'
 import theme from './utils/theme'
 import './App.scss'
-import Registration from './routes/Registration';
 
 class App extends React.Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <MainLayout>
-          <Registration />
-        </MainLayout>
+        <MainLayout />
       </ThemeProvider>
     )
   }
